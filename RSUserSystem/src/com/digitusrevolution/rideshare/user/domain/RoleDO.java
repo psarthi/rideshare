7b630fc2514d190f4a828840fe775c6f301a2f07
Partha Sarthi <partha.sarthi@gmail.com>
2015-11-26 17:50:16 +0530

    Minor Update

---
+++ RSUserSystem/src/com/digitusrevolution/rideshare/user/domain/RoleDO.java
@@ -0,0 +1,59 @@
+package com.digitusrevolution.rideshare.user.domain;
+
+import com.digitusrevolution.rideshare.common.DomainDataMapper;
+import com.digitusrevolution.rideshare.model.user.data.RoleEntity;
+import com.digitusrevolution.rideshare.model.user.domain.Role;
+
+public class RoleDO implements DomainDataMapper{
+
+	private Role role;
+	private RoleEntity roleEntity;
+
+	public RoleDO() {
+		setRole(new Role());
+		roleEntity = new RoleEntity();
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public RoleEntity getRoleEntity() {
+		return roleEntity;
+	}
+
+
+	public void setRoleEntity(RoleEntity roleEntity) {
+		this.roleEntity = roleEntity;
+	}
+
+
+	@Override
+	public void mapDomainModelToDataModel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mapDataModelToDomainModel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mapChildDataModelToDomainModel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mapChildDomainModelToDataModel() {
+		// TODO Auto-generated method stub
+
+	}
+}
