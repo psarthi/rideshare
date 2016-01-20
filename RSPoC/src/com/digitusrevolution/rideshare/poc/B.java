b1081f6de6caea12a94c9c36ceb561fe9abc6baa
Partha Sarthi <partha.sarthi@gmail.com>
2016-01-20 18:35:52 +0530

    Hibernate PoC classes

---
+++ RSPoC/src/com/digitusrevolution/rideshare/poc/B.java
@@ -0,0 +1,50 @@
+package com.digitusrevolution.rideshare.poc;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="B")
+public class B {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof B)) {
+			return false;
+		}
+		B other = (B) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
+}
---
+++ RSPoC/src/com/digitusrevolution/rideshare/poc/HibernateTest.java
@@ -0,0 +1,59 @@
+package com.digitusrevolution.rideshare.poc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.digitusrevolution.rideshare.common.db.GenericDAOImpl;
+import com.digitusrevolution.rideshare.common.db.HibernateUtil;
+
+public class HibernateTest {
+	
+	private static final Logger logger = LogManager.getLogger(HibernateTest.class.getName());
+	
+	public static void main(String[] args) {
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction transation = null;	
+		try {
+			transation = session.beginTransaction();
+
+			GenericDAOImpl<A, Integer> genericDAOImplA = new GenericDAOImpl<>(A.class);
+			GenericDAOImpl<B, Integer> genericDAOImplB = new GenericDAOImpl<>(B.class);
+//			for (int i=0;i<5;i++){
+//				int id = genericDAOImplA.create(new A());
+//				System.out.println("Created Id:"+id);
+//				id = genericDAOImplB.create(new B());
+//				System.out.println("Created Id:"+id);				
+//			}
+//			B b1 = genericDAOImplB.get(5);
+			B b2 = genericDAOImplB.get(6);
+			A a = genericDAOImplA.get(5);
+//			a.getbCollection().add(b1);
+			a.getbCollection().remove(b2);
+			genericDAOImplA.update(a);
+
+			transation.commit();
+
+			/*
+			 * Reason for catching RuntimeException and not HibernateException as all exceptions thrown by Hibernate
+			 * is not of type HibernateException such as NotFoundException
+			 */
+		} catch (RuntimeException e) {
+			if (transation!=null){
+				logger.error("Transaction Failed, Rolling Back");
+				transation.rollback();
+				throw e;
+			}
+		}
+		finally {
+			if (session.isOpen()){
+				logger.info("Closing Session");
+				session.close();				
+			}
+		}	
+		
+	}
+
+}
